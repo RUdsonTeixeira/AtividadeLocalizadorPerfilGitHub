7e24a006464e523c43065aaa2383b3a41127ab93
Rudson Teixeira <rudson.teixeira@estudante.ifms.edu.br>
2022-12-09 00:37:53 -0400

    API e Busca

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,58 @@
-import axios from 'axios'
 import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+
+type GITHUBResponse = {
+  name: string;
+  avatar_url: string;
+  bio: string;
+};
+
 function App() {
-  return(
+  const [userName, setUserName] = useState("");
+  const [name, setName] = useState("Aguardando...");
+  const [bio, setBio] = useState("Aguardando...");
+  const [avatarURL, setAvatarURL] = useState("Aguardando...");
+
+  const handleSearch = () => {
+    axios
+      .get<GITHUBResponse>(`https://api.github.com/users/${userName}`)
+      .then((res) => {
+        setName(res.data.name);
+        setBio(res.data.bio);
+        setAvatarURL(res.data.avatar_url);
+      })
+  };
+
+  return (
     <div className="container-app">
       <div className="container">
         <header className="header-top">
-          <h1>
-            Buscador de perfis
-          </h1>
+          <ul>
+            <li>Jovem Programador</li>
+          </ul>
         </header>
-
         <main>
           <div className="form">
-            <input type="text" placeholder="Digite o nome de usuario"/>
-            <button>Buscar</button>
+            <h1>Buscador de Perfis do GITHUB</h1>
+            <input
+              type="text"
+              placeholder="Digite um username"
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <button onClick={handleSearch}>Buscar</button>
           </div>
           <div className="content">
-            <img src="https:" alt="" />
-            <h1>Nome do Perfil</h1>
-            <p>
-              Bio do Perfil
-            </p>
+            <div>
+              <img src={avatarURL} alt="Perfil" />
+              <h1>{name}</h1>
+              <p>{bio}</p>
+            </div>
           </div>
         </main>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
